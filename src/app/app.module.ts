564704829cd20c10d6024d7d49c04d926fb0f715
@vahe1995 <vahe.ghonakhchyan@innline.am>
2021-11-04 12:24:22 +0400

    move categories component files to folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CategoriesComponent } from './views/categories.component';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
